453c0ba18add187abea8caa49ec7d3b982bda75d
QHF <cnqian1996@gmail.com>
2023-06-08 18:52:36 +0800

    日常更新

---
+++ 1、函数/15、闭包（2）.py
@@ -0,0 +1,16 @@
+def func(a,b):
+    c = 10
+
+    def inner_func():
+        s = a + b + c
+        print("相加之后的结果是：",s)
+
+    return inner_func
+
+
+
+# 调用：
+func(6,9)()
+x = func(6,9) # x 就是 inner_func
+x()
+print(x)
